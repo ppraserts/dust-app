8772d856012b21f11038b2c9f10e7508fb280d46
ppraserts <ppraserts@gmail.com>
2019-04-06 23:59:42 +0700

    add realtime

--- src/api-services/stations.service.js
+++ src/api-services/stations.service.js
@@ -1,9 +1,11 @@
-// import Axios from 'axios'
-// const RESOURCE_NAME = '/all.json'
+import Axios from 'axios'
+const RESOURCE_NAME = '/stations'
 // import json from '../data/test.json'
 export default {
-  getAll: function () {
-    // debugger
-    return ''
+  getAll () {
+    return Axios.get(RESOURCE_NAME)
+  },
+  get (id, type) {
+    return Axios.get(`${RESOURCE_NAME}/${id}/${type}`)
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import Axios from 'axios'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
-/* Axios.defaults.baseURL = 'http://localhost:8080/assets' */
+Axios.defaults.baseURL = 'https://agile-reaches-83574.herokuapp.com/api'
 
 /* eslint-disable no-new */
 new Vue({
